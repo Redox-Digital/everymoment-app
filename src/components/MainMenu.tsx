c464277d11c770e455c4f474e518f468e3b78b03
Mikaël Ruffieux <ruffieux.mikael@gmail.com>
2022-05-29 13:06:30 +0200

    Adding static Mobile Menu

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
 
 export default function MainMenu() {
-  return <nav>menu</nav>;
+  /* Animation Burger  : https://codepen.io/elitsa_dimitrova/pen/LJWBVo  */
+  return (
+    <nav>
+      <div className="menu">
+        <div className="menuBurger">
+          <a href="#">
+            <span className="burger" />
+            <span className="burger" />
+            <span className="burger" />
+            <span className="iconLabel">Menu</span>
+          </a>
+        </div>
+        <div className="menuLeft">
+          <a href="#">Vêtements</a>
+          <a href="#">Accessoires</a>
+        </div>
+        <div className="menuMid">
+          <img src="https://picsum.photos/400/42" alt="Every Moment" />
+        </div>
+        <div className="menuRight">
+          <div className="menuRight__links">
+            <a href="#">À propos</a>
+            <a href="#">Journal</a>
+          </div>
+          <div className="menuRight__pictograms">
+            <a href="#">Compte</a>
+            <a href="#">Panier</a>
+          </div>
+        </div>
+      </div>
+      <span className="menuDecoration" />
+    </nav>
+  );
 }
